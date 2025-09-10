84385373e5c56029307b27da8e57a4984c9dd8d5
bakeel-ali <bakeelaledrisy@gmail.com>
2025-09-10 10:53:08 +0300

    Update settings.py
    retern to SQLite

--- iris_backend/settings.py
+++ iris_backend/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = "iris_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
     }
